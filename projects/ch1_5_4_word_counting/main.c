6e79bad157815f99d3c0317bd6f15b0f3c926e4a
Duda Bogdan <bogdan.duda.systems@proton.me>
2026-06-30 22:18:47 +0300

    ch1: 5.4, added the missing exercise condition

--- projects/ch1_5_4_word_counting/main.c
+++ projects/ch1_5_4_word_counting/main.c
@@ -15,13 +15,16 @@ int main (void) {
             if (state == OUT) {
                 state = IN;
                 word_count++;
+                if (word_count > 1) { putchar('\n'); }
             }
+
+            putchar(c);
     
         } else { state = OUT; }
 
     }
 
-    if (printf ("%d\n", word_count) < 0) { return 1; }
+    if (printf ("\n%d\n", word_count) < 0) { return 1; }
 
     return 0;
 }
