a2a8a9ffd7bc5b1bfe73494f03230c46658eef30
Duda Bogdan <bogdan.duda.systems@proton.me>
2026-07-01 17:28:38 +0300

    ch1: modified input text for sub-chapter 5 part 4

--- chapter_1/ch1_5_4_word_counting/run.sh
+++ chapter_1/ch1_5_4_word_counting/run.sh
@@ -10,7 +10,7 @@ echo "=== Target: Executing Binary ==="
 echo "--------------------------------"
 
 if [ -f "app" ]; then
-    echo -e "Write a     \n\n    program to \t copy it\bs input \n to \t its output, re\tplacing each         string of \tone or m\nore blanks b\n\t a single b\blank.\n" > test_input.txt
+    echo -e "Write a   \t\t\t  \n\n    program to \t copy it\bs input \n to \t its output, re\tplacing each         string of \tone or m\nore blanks b\n\t a single b\blank.\n" > test_input.txt
     ./app < test_input.txt
     rm test_input.txt
 else 
